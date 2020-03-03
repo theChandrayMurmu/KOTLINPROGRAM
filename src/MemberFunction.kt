181777055ad71f410c2bb319cd07d844c329316d
Chandray Murmu <chandraymurmu7@gmail.com>
2020-03-03 15:33:04 +0530

    Add Constructor Program with Initializer.

--- src/MemberFunction.kt
+++ src/MemberFunction.kt
@@ -4,16 +4,22 @@
 
 
 
-class Account
-{
+class Account {
     var acc_no: Int = 0
     var name: String = ""
     var amount: Float = 0.toFloat()
-    fun insert(ac: Int, n:String, am:Float)
-    {
+    fun insert(ac: Int, n: String, am: Float) {
         acc_no = ac
         name = n
         amount = am
         println("Account no:$")
     }
+
+}
+
+fun main(args: Array<String>) {
+    Account()
+    val acc = Account()
+    acc.insert(76753, "Mr.Moon", 9999999f) //accessing member function
+    println("${acc.name}") //accessing class property
 }
